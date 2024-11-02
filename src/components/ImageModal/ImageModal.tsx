5109078e7371ac347d2248fe41e2c4a8d755cd7f
Nykyforuk Vasyl <nikiforuk.vasya@gmail.com>
2024-11-02 21:03:13 +0200

    add

--- src/components/ImageModal/ImageModal.tsx
+++ src/components/ImageModal/ImageModal.tsx
@@ -14,30 +14,28 @@ const ImageModal = ({ modalIsOpen, closeModal, currentImage }: Props) => {
 
   return (
     <>
-      <>
-        <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          style={{
-            content: {
-              top: "50%",
-              left: "50%",
-              right: "auto",
-              bottom: "auto",
-              marginRight: "-50%",
-              transform: "translate(-50%, -50%)",
-              padding: "0px",
-              border: "none",
-              backgroundColor: "transparent",
-            },
-            overlay: {
-              backgroundColor: "rgba(0, 0, 0, 0.75)",
-            },
-          }}
-        >
-          <img src={currentImage} alt="Full-size" className={s.modalImage} />
-        </Modal>
-      </>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={{
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+            padding: "0px",
+            border: "none",
+            backgroundColor: "transparent",
+          },
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.75)",
+          },
+        }}
+      >
+        <img src={currentImage} alt="Full-size" className={s.modalImage} />
+      </Modal>
     </>
   );
 };
