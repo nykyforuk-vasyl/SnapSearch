f97c31e1c73cb13bca10f96b52fc6ce69f31356a
Nykyforuk Vasyl <nikiforuk.vasya@gmail.com>
2024-11-03 14:01:58 +0200

    add

--- src/search-img-api.ts
+++ src/search-img-api.ts
@@ -4,8 +4,25 @@ const API_KEY = "GDY5kFttlZ6do23iVYxc0ZSjVa2mNhSFnGmCjVV8mnQ";
 
 axios.defaults.baseURL = `https://api.unsplash.com/`;
 
-const searchImagesForTopic = async (topic, page) => {
-  const res = await axios.get(
+type serverObject = {
+  id: string;
+  likes: number;
+  alt_description: string;
+  urls: {
+    regular: string;
+    small: string;
+  };
+};
+
+type ApiResponse = {
+  results: serverObject[];
+};
+
+const searchImagesForTopic = async (
+  topic: string,
+  page: number
+): Promise<serverObject[]> => {
+  const res = await axios.get<ApiResponse>(
     `search/photos?client_id=${API_KEY}&query=${topic}&per_page=8&page=${page}`
   );
 
